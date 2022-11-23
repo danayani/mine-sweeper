60d69d6bae692a81d6a5475f2b8a90412eccabac
danayani <118884838+danayani@users.noreply.github.com>
2022-11-23 21:04:34 +0200

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,19 +1,57 @@
 'use strict'
 var gBoard
 var gLevel
+var gMinesOnBoard
+var gCellCount
+var gMyTimerId
+var gTimer
+var isGameOn
+
+var gGame
 
 // const BOOM = '#'
 // const FOOD = '○'
 // const EMPTY = ' '
 const MINE = '💣'
+const FLAG = '🚩'
+
+
 
 function initGame() {
     startLevel()
+    gCellCount = 0
+    gMyTimerId = null
+    gTimer = 0
     gBoard = startBoard()
+    gMinesOnBoard = gBoard.length
+    //gMinesOnBoard = 0
+    getMinesOnBoard(gMinesOnBoard)
     setMinesNegsCount()
 
     renderBoard(gBoard)
     console.log(gBoard)
+    console.log(gCellCount)
+
+    
+}
+function startGame(){
+    gGame = {
+        isOn: false,
+        shownCount: 0,
+        markedCount: 0,
+        secsPassed: 0
+       }
+       
+}
+
+function gameOver(isWin){
+    if(!isWin) showMines()
+    console.log('win ?', isWin)
+    clearInterval(gMyTimerId)
+}
+
+function checkWin(){
+    if(gCellCount === 0) gameOver(true)
 }
 
 function startBoard() {
@@ -23,25 +61,29 @@ function startBoard() {
     for (var i = 0; i < size; i++) {
         board.push([])
         for (var j = 0; j < size; j++) {
+            gCellCount++
             var curr = {
                 minesAroundCount: 0,
                 isShown: false,
                 isMine: false,
-                isMarked: true
+                isMarked: false
             }
             board[i].push(curr)
         }
     }
-
-    var indexI = getRandomInt(0, size)
-    var indexJ = getRandomInt(0, size)
-    board[indexI][indexJ].isMine = true
-    indexI = getRandomInt(0, size)
-    indexJ = getRandomInt(0, size)
-    board[indexI][indexJ].isMine = true
-    
-
     return board
+}
+
+function getMinesOnBoard(amount){
+    
+    for(var z = 0; z < amount;z++){
+        gCellCount--
+        var ramdomCell = getEmptyRamdomCell()
+        var indexI = ramdomCell.i
+        var indexJ = ramdomCell.j
+
+        gBoard[indexI][indexJ].isMine = true
+    }
 }
 
 function startLevel() {
@@ -61,16 +103,59 @@ function setMinesNegsCount() {
     }
 }
 
-function cellClicked(i,j,elCell,evCell){
+function showNegs(cellI, cellJ, mat = gBoard) {
+
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= mat.length) continue
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= mat[i].length) continue
+            if (!mat[i][j].isShown){
+                gCellCount--
+                mat[i][j].isShown = true
+            } 
+        }
+    }
+}
+
+function cellClicked(i,j){
+    if(!gMyTimerId) gMyTimerId = setInterval(myTimer,1000)
     var curr = gBoard[i][j]
 
-    curr.isShown = true
-    showNegs(i,j)
+    if(curr.isMine) mineExpload()
+    if(curr.isShown) return
+    if(curr.minesAroundCount > 0){
+        curr.isShown = true
+        gCellCount--
+    }
+    if(curr.minesAroundCount === 0)showNegs(i,j)
+    
     renderBoard(gBoard)
-
-    console.log(evCell)
-
 }
+
+function whichButton(i, j, evCell){
+    
+    if(evCell.button === 2){
+        gBoard[i][j].isMarked = true
+        renderBoard()
+    } 
+}
+
+function mineExpload(){
+    gameOver(false)
+}
+
+function showMines(){
+
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard.length; j++) {
+            var curr = gBoard[i][j]
+            if(curr.isMine) curr.isShown = true 
+        }
+    }
+    renderBoard()
+}
+
+
 
 
 
--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,6 @@
 'use strict'
 
-function renderBoard(mat, selector = '.board-container') {
+function renderBoard(mat = gBoard, selector = '.board-container') {
 
     var strHTML = '<table border="0"><tbody>'
     for (var i = 0; i < mat.length; i++) {
@@ -12,18 +12,30 @@ function renderBoard(mat, selector = '.board-container') {
             var className = `cell cell-${i}-${j}`
             if (curr.isShown) {
                 className += ` shown`
+            } else if (curr.isMarked) {
+                cell = FLAG
             } else {
                 cell = ''
             }
-            strHTML += `<td class="${className}" onclick="cellClicked(${i},${j},this,event)">${cell}</td>`
+
+            strHTML += `<td class="${className}" onclick="cellClicked(${i},${j})" onmousedown="whichButton(${i},${j},event)">${cell}</td>`
         }
 
         strHTML += '</tr>'
     }
     strHTML += '</tbody></table>'
 
-    const elContainer = document.querySelector(selector)
+    const elContainer = document.querySelector(selector) // update board
     elContainer.innerHTML = strHTML
+
+    const eltxtMines = document.querySelector('.txtMinesOnBoard span')// update Mains left
+    eltxtMines.innerText = gMinesOnBoard
+
+    const eltxtCellCount = document.querySelector('.txtCellCount span')// update empty cell to open
+    eltxtCellCount.innerText = gCellCount
+
+    checkWin()
+
 }
 
 function countMinesNegs(cellI, cellJ, mat) {
@@ -38,17 +50,6 @@ function countMinesNegs(cellI, cellJ, mat) {
         }
     }
     return negsCount
-}
-
-function showNegs(cellI, cellJ, mat = gBoard) {
-
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (j < 0 || j >= mat[i].length) continue
-            mat[i][j].isShown = true
-        }
-    }
 }
 
 //not working at the moment
@@ -70,6 +71,28 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getEmptyRamdomCell() {
+    var emptyCells = []
+
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard.length; j++) {
+            var curr = gBoard[i][j]
+            if (!curr.isMine) emptyCells.push({ i: i, j: j })
+        }
+    }
+
+    var randomCell = emptyCells[getRandomInt(0, emptyCells.length)] //return ex {i: 2, j: 4}
+    return randomCell
+}
+
+function myTimer(){
+    if(gTimer > 5000) gameOver(false)
+    gTimer++
+
+    const eltxtMyTimer = document.querySelector('.myTimer')// update myTimer
+    eltxtMyTimer.innerText = gTimer
 }
 
 
